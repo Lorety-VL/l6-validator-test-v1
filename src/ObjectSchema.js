1e38b6dd1b6ddf2a0090587601bc76a36d5101d0
Lorety-VL <vladimirking18@gmail.com>
2023-12-24 22:00:48 +0300

    all completed

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -11,6 +11,17 @@ export default class ObjectSchema {
     const keys1 = Object.keys(this.schema);
     const keys2 = Object.keys(value);
     if (keys2.length !== keys1.length) return false;
-    return keys1.every((key) => this.schema[key].isValid(value[key]));
+    // return keys1.every((key) => this.schema[key].isValid(value[key]));
+    return keys1.every((key) => {
+      if (
+        typeof value[key] === 'object'
+        && !Array.isArray(value[key])
+        && value[key] !== null
+      ) {
+        const schema = new ObjectSchema(this.schema[key]);
+        return schema.isValid(value[key]);
+      }
+      return this.schema[key].isValid(value[key]);
+    });
   }
 }
